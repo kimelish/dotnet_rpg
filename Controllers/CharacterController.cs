44ef5810ce5e5acaafc40fe61a3b16bd5f6bde96
kimelish <yoshuakim@gmail.com>
2020-04-16 09:51:25 -0700

    Added RouteID

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,16 +1,25 @@
+using System.Linq;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace dotnet_rpg.Models 
-{
+namespace dotnet_rpg.Models {
     [ApiController]
-    [Route("[controller]")]
-    public class CharacterController : ControllerBase
-    {
-        private static Character knight = new Character();
+    [Route ("[controller]")]
+    public class CharacterController : ControllerBase {
+        private static Character knight = new Character ();
+        private static List<Character> characters = new List<Character> {
+            new Character (),
+            new Character { Id = 1, Name = "Sam" }
+        };
 
-        public IActionResult Get ()
-        {
-            return Ok(knight);
+        [HttpGet("GetAll")]
+        public IActionResult Get () {
+            return Ok (characters);
+        }
+
+        [Route("{id}")]
+        public IActionResult GetSingle (int id) {
+            return Ok (characters.FirstOrDefault(c => c.Id == id));
         }
 
     }
