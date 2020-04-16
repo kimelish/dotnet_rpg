92a5a6a807e6e7a5d8d42f2380c7480897cbfac7
kimelish <yoshuakim@gmail.com>
2020-04-16 08:16:08 -0700

    InitialCreate

---
+++ Models/Character.cs
@@ -0,0 +1,9 @@
+namespace dotnet_rpg.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+    }
+}
