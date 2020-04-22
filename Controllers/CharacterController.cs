7d70de27a47762bb8aa8087acb5e4d68517f149d
kimelish <yoshuakim@gmail.com>
2020-04-22 07:30:51 -0700

    added user specific authentication

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,65 +1,69 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Services;
 using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Models
 {
+    [Authorize]
     [ApiController]
-    [Route("[controller]")]
+    [Route ("[controller]")]
     public class CharacterController : ControllerBase
     {
         private readonly ICharacterService _characterService;
 
-        public CharacterController(ICharacterService characterService)
+        public CharacterController (ICharacterService characterService)
         {
             _characterService = characterService;
         }
 
-        [HttpGet("GetAll")]
-        public async Task<IActionResult> Get()
+        [HttpGet ("GetAll")]
+        public async Task<IActionResult> Get ()
         {
-            return Ok(await _characterService.GetAllCharacters());
+            int userId = int.Parse (User.Claims.FirstOrDefault (c => c.Type == ClaimTypes.NameIdentifier).Value);
+            return Ok (await _characterService.GetAllCharacters (userId));
         }
 
-        [Route("{id}")]
-        public async Task<IActionResult> GetSingle(int id)
+        [Route ("{id}")]
+        public async Task<IActionResult> GetSingle (int id)
         {
-            return Ok(await _characterService.GetCharacterById(id));
+            return Ok (await _characterService.GetCharacterById (id));
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddCharacter(AddCharacterDto newCharacter)
+        public async Task<IActionResult> AddCharacter (AddCharacterDto newCharacter)
         {
-            return Ok(await _characterService.AddCharacter(newCharacter));
+            return Ok (await _characterService.AddCharacter (newCharacter));
 
         }
 
         [HttpPut]
-        public async Task<IActionResult> UpdateCharacter(UpdateCharacterDto updatedCharacter)
+        public async Task<IActionResult> UpdateCharacter (UpdateCharacterDto updatedCharacter)
         {
-            ServiceResponse<GetCharacterDto> response = await _characterService.UpdateCharacter(updatedCharacter);
+            ServiceResponse<GetCharacterDto> response = await _characterService.UpdateCharacter (updatedCharacter);
 
             if (response.Data == null)
             {
-                return NotFound(response);
+                return NotFound (response);
             }
 
-            return Ok(response);
+            return Ok (response);
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
+        [HttpDelete ("{id}")]
+        public async Task<IActionResult> Delete (int id)
         {
-            ServiceResponse<List<GetCharacterDto>> response = await _characterService.DeleteCharacter(id);
+            ServiceResponse<List<GetCharacterDto>> response = await _characterService.DeleteCharacter (id);
             if (response.Data == null)
             {
-                return NotFound(response);
+                return NotFound (response);
             }
-            return Ok(response);
+            return Ok (response);
         }
 
     }
